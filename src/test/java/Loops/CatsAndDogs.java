5a38791ebbae6ec3d183082e4185097100a2ea54
Ahmtfrks <lusiferc679@gmail.com>
2024-04-13 00:08:50 +0200

    CatsAndDogs LOOP

---
+++ src/test/java/Loops/CatsAndDogs.java
@@ -0,0 +1,31 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class CatsAndDogs {
+    public static void main(String[] args) {
+        //DO NOT TOUCH FOLLOWING LINE/LINES
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+
+        //WRITE YOUR CODE BELOW
+        int cat=0;
+        int dog=0;
+        for(int i=0;i<str.length()-2;i++){
+            String input= str.toLowerCase().substring(i,i+3);
+
+
+            if(input.equals("cat")){
+                cat++;
+            }else if(input.equals("dog")){
+                dog++;
+            }
+        }
+        if(cat==dog){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+
+}
